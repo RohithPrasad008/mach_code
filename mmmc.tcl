58c79ea652b489c6835cd4f7cec998cb7e94ceb4
RohithPrasad008 <68744411+RohithPrasad008@users.noreply.github.com>
2020-07-25 12:11:12 +0530

    MMMC - 1MHz

--- mmmc.tcl
+++ mmmc.tcl
@@ -1,7 +1,9 @@
+#################mmmc_file_which_defines_multiple corners_for_the_design#####################
 
 #################best_case###################
 
-create_rc_corner -name best -preRoute_res {1.0} -preRoute_cap {1.0} -preRoute_clkres {0.0} -preRoute_clkcap {0.0} -postRoute_res {1.0} -postRoute_cap {1.0} -postRoute_clkres {0.0} -postRoute_clkcap {0.0} -qx_tech_file {/home/sois/Desktop/Rohith/RAK_18.1_blockImplementation/LIBS/qx/qrcTechFile}
+create_rc_corner -name best -preRoute_res {1.0} -preRoute_cap {1.0} -preRoute_clkres {0.0} -preRoute_clkcap {0.0} -postRoute_res {1.0}\
+-postRoute_cap {1.0} -postRoute_clkres {0.0} -postRoute_clkcap {0.0} -qx_tech_file {/home/sois/Desktop/Rohith/RAK_18.1_blockImplementation/LIBS/qx/qrcTechFile}
 
 create_op_cond -name best -library_file {/home/sois/Desktop/Rohith/RAK_18.1_blockImplementation/LIBS/lib/min/fast.lib} -P (1) -V (1.2) -T (-40)
 create_library_set -name best -timing {/home/sois/Desktop/Rohith/RAK_18.1_blockImplementation/LIBS/lib/min/fast.lib}
@@ -10,7 +12,9 @@ create_delay_corner -name best -library_set best -rc_corner best
 
 #################worst_case#################
 
-create_rc_corner -name worst -preRoute_res {1.1} -preRoute_cap {1.1} -preRoute_clkres {1.1} -preRoute_clkcap {1.1} -postRoute_res {1.1} -postRoute_cap {1.1} -postRoute_clkres {1.1} -postRoute_clkcap {1.1} -qx_tech_file {/home/sois/Desktop/Rohith/RAK_18.1_blockImplementation/LIBS/qx/qrcTechFile}
+create_rc_corner -name worst -preRoute_res {1.1} -preRoute_cap {1.1} -preRoute_clkres {1.1} -preRoute_clkcap {1.1}\
+-postRoute_res {1.1} -postRoute_cap {1.1} -postRoute_clkres {1.1} -postRoute_clkcap {1.1}\
+-qx_tech_file {/home/sois/Desktop/Rohith/RAK_18.1_blockImplementation/LIBS/qx/qrcTechFile}
 
 create_op_cond -name worst -library_file {/home/sois/Desktop/Rohith/RAK_18.1_blockImplementation/LIBS/lib/max/slow.lib} -P (1) -V (0.9) -T (125)
 create_library_set -name worst -timing {/home/sois/Desktop/Rohith/RAK_18.1_blockImplementation/LIBS/lib/max/slow.lib}
